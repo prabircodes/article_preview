8b14d097d5b7de106e05654286f2defda61cb94c
Prabir Singh <codeprabir@gmail.com>
2022-10-27 00:25:59 +0530

    done

---
+++ app.js
@@ -0,0 +1,5 @@
+const shareOption = document.querySelector('.share-option')
+document.querySelector('.share-button').addEventListener('click', function () {
+  this.classList.toggle('active')
+  shareOption.classList.toggle('active')
+})
